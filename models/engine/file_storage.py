74651dd271bb0ed305d5cb286df486709105662f
dakhamohammed <isanagy@live.fr>
2023-12-20 08:32:16 -0800

    Fix file_storage.py requirement error.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,6 +56,6 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete object from a list of objects."""
-        if obj is not None:
+        if obj:
             obj_to_delete = f'{type(obj).__name__}.{obj.id}'
             del self.__objects[obj_to_delete]
