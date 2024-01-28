d77fe93323c08da87ed59377b04fae4707bdb999
dakhamohammed <isanagy@live.fr>
2024-01-28 12:24:13 +0100

    Script that starts a Flask web application.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Script that starts a Flask web application."""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
+	"""root route that display simple text."""
 	return 'Hello HBNB!'
 
 
