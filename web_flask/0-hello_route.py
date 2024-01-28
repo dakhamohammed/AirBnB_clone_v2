a22c0223d4a02d6d992fa022ccc64b02191e1623
dakhamohammed <isanagy@live.fr>
2024-01-28 15:41:46 +0100

    cript that starts a Flask web application.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
 	"""root route that display simple text."""
 	return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
 
