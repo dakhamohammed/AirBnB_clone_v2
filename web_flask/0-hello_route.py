ee2a78f5d05228187e562ae780f1d8f3a7230a53
dakhamohammed <isanagy@live.fr>
2024-01-28 15:53:51 +0100

    Script that starts a Flask web application.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application.
+script that starts a flask web application.
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -11,6 +12,6 @@ def index():
 	"""display simple text."""
 	return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-
